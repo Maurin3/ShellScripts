4121076219fcda844a8d9d327fe7cbb7d82cc757
Maurin3 <maurin3_1994@msn.com>
2020-06-23 14:47:52 +0200

    [IMP] Improve odoo deploy
    Improve the odoo deploy option by checking if there is a module to deploy
    (the 2nd argument)

--- odoo/scripts/deploy.sh
+++ odoo/scripts/deploy.sh
@@ -21,23 +21,12 @@ if [[ -z $2 ]]; then
     error "There is nothing to deploy on the database $1"
 fi
 
-cd $ODOO_PATH/odoo
-branch=$(git symbolic-ref --short HEAD)
-cd $current_dir
-sql="SELECT latest_version FROM ir_module_module WHERE name='base'"
-version=`psql -t -A -c "$sql" $1 2> /dev/null`
-version=${version%.*.*}
-if [[ $branch != $version ]]; then
-    error "The versions between Odoo and the database are not the same. (Odoo : $branch - Database: $version)"
+if [[ ! $2 ]]; then 
+    error "There is nothing to deploy. Check you command..."
     exit 1
 else
     info "Deploying module to Odoo on database $1"
-    if [[ ! $2 ]]; then 
-        error "There is nothing to deploy. Check you command..."
-        exit 1
-    else
-        $ODOO_PATH/odoo/odoo-bin deploy $2 --db $1
-    fi
+    $ODOO_PATH/odoo/odoo-bin deploy $2 --db $1
 fi
 
 complete
