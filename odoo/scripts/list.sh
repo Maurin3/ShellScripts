543153f283cd3128812185f1024f61fdaa97141d
Maurin3 <maurin3_1994@msn.com>
2020-07-22 21:38:43 +0200

    [IMP] Make odoo list better (filestore)

--- odoo/scripts/list.sh
+++ odoo/scripts/list.sh
@@ -15,6 +15,8 @@ do
 done
 printf '\n'
 
+cur_dir=$(pwd)
+
 for db in $list
 do
     sql="SELECT latest_version FROM ir_module_module WHERE name='base'"
@@ -24,13 +26,22 @@ do
         version="No"
     fi
     if [[ -d ~/.local/share/Odoo/filestore/$db ]]; then
-        filestore="✔"
-        printf "%-20s | %-12s | %b %-20s %b \n" $db $version $LIGHT_GREEN $filestore $NC
+        cd ~/.local/share/Odoo/filestore/$db
+        res=$(ls -A | wc -l)
+        if [ $res -gt 100 ]; then
+            filestore="✔"
+            printf "%-20s | %-12s | %b %-20s %b \n" $db $version $LIGHT_GREEN $filestore $NC
+        else
+            filestore='✘'
+            printf "%-20s | %-12s | %b %-20s %b \n" $db $version $RED $filestore $NC
+        fi
     else
-        filestore=''
-        printf "%-20s | %-12s | %-20s\n" $db $version $filestore
+        filestore='✘'
+        printf "%-20s | %-12s | %b %-20s %b \n" $db $version $RED $filestore $NC
     fi
 done
+
+cd $cur_dir
 
 echo
 
